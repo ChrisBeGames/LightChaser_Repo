3b501137c81d0bf1534f68ed6236c1303c1ddaa3
gamejam <gamejam@coleman.local>
2016-01-30 12:00:36 -0800

    keyboard rowing

--- LightChaser_GGJ16/Assets/_Scripts/KeyboardMovement.cs
+++ LightChaser_GGJ16/Assets/_Scripts/KeyboardMovement.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class KeyboardMovement : MonoBehaviour {
 
+	public Rigidbody player;
+
 	void Start () {
-	
+		if (player == null) {
+			player = GameObject.FindGameObjectWithTag ("Player").GetComponentInChildren<Rigidbody> ();
+		}
 	}
 	
 	void Update () {
 		float left = Input.GetAxis ("Row Left");
 		float right = Input.GetAxis ("Row Right");
+		float intent = Input.GetAxis ("Intent");
 
+		Vector3 leftPos = transform.TransformPoint (new Vector3 (-1, 0, 0));
+		Vector3 rightPos = transform.TransformPoint (new Vector3 (1, 0, 0));
 
+		player.AddForceAtPosition (transform.forward * (intent * left), leftPos);
+		player.AddForceAtPosition (transform.forward * (intent * right), rightPos);
 	}
 }

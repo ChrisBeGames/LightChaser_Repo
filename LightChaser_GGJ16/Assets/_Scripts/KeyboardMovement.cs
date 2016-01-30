5927ca3e405f216efc2befe708fae58050178390
gamejam <gamejam@coleman.local>
2016-01-30 11:25:24 -0800

    beautiful

---
+++ LightChaser_GGJ16/Assets/_Scripts/KeyboardMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardMovement : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		float left = Input.GetAxis ("Row Left");
+		float right = Input.GetAxis ("Row Right");
+
+
+	}
+}

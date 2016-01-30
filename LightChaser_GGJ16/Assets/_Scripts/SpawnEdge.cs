360a8e3eebea1becb2b58b949ffc84ae9f2a4be8
Stralor <gamejam@coleman.local>
2016-01-30 14:04:09 -0800

    Water's Edge and tweaks

---
+++ LightChaser_GGJ16/Assets/_Scripts/SpawnEdge.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**Lazy af. */
+public class SpawnEdge : MonoBehaviour {
+
+	public GameObject edgePrefab;
+	public float prefabWidth, radiusToPopulate;
+
+	void Start () {
+//		for (int i = 0; i < (int)(Mathf.PI * 2 * radiusToPopulate / prefabWidth); i++) {
+//			Instantiate(edgePrefab, new Vector3(
+//		}
+	
+		float degreeOffset = 360.0f / (Mathf.PI * 2 * radiusToPopulate / prefabWidth);
+
+		Vector3 tempPosition = Vector3.zero;
+
+		for (float currentDegree = 0; currentDegree < 360; currentDegree += degreeOffset) {
+
+			tempPosition.x= Mathf.Cos (currentDegree) * radiusToPopulate;
+			tempPosition.z= Mathf.Sin (currentDegree) * radiusToPopulate;
+
+			var tempObj = (GameObject)Instantiate (edgePrefab);
+			tempObj.transform.position = tempPosition + transform.position;
+			tempObj.transform.rotation = Quaternion.LookRotation(transform.position - tempObj.transform.position);
+			tempObj.transform.SetParent (transform);
+		}
+	}
+}

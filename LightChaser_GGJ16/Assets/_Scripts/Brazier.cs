329a77f09f775b789813aca76c7bc7c5cb6f4d22
Nils Olsson <nilso@enosis.net>
2016-01-30 16:56:14 -0800

    Add brazier

---
+++ LightChaser_GGJ16/Assets/_Scripts/Brazier.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brazier : MonoBehaviour {
+
+	public bool isLit;
+	public ParticleSystem particleSystem;
+
+	void OnTriggerEnter (Collider other) {
+		print ("Enter");
+		if (other.tag == "Ignitor") {
+			isLit = true;
+			particleSystem.enableEmission = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		print ("Exit");
+		if (other.tag == "Ignitor") {
+			isLit = false;
+			particleSystem.enableEmission = false;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (particleSystem) {
+//			if (isLit && !particleSystem.enableEmission) {
+//				OnTriggerEnter (GameObject.FindGameObjectWithTag ("Ignitor").GetComponent<Collider> ());
+//			} else if (!isLit && particleSystem.enableEmission) {
+//				particleSystem.enableEmission = false;
+//			}
+//		}
+	}
+}
